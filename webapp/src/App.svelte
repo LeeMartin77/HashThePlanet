a77d44acd3c733be08cb594527aa08a65a58aa89
Lee Martin <hello@leejohnmartin.co.uk>
2023-11-25 21:10:30 +0000

    visual:@ better.

--- webapp/src/App.svelte
+++ webapp/src/App.svelte
@@ -6,7 +6,7 @@
   import {
     Header, Button,
     Grid, Row, Column,
-    HeaderGlobalAction, SideNav, SideNavItems, Content,
+    HeaderAction, SideNav, SideNavItems, Content,
      SideNavMenuItem, Theme
 
 } from 'carbon-components-svelte';
@@ -17,24 +17,24 @@ const menuViews: Views[] = ['Home', 'Upgrades']
 
 
 <Theme theme='g100'>
-  <Header company="HTP2000cx.VMCT.BNG12" isSideNavOpen>
-    <slot name="platform">
-      <div style="padding-left: 1em;">{$hashCount.toFixed(2)}</div>
-      <div style="padding-left: 1em;">{$hashRate.toFixed(2)}/s</div>
-    </slot>
-    <slot name="skip-to-content">
-      <HashButton />
-    </slot>
+  <Header>
+    <span slot="company">
+      HTP2000cx.VMCT.BNG12
+    </span>
+    <span slot="platform">
+      <div style="padding-left: 1em;">{$hashCount.toFixed(2)} {$hashRate.toFixed(2)}/s</div>
+    </span>
+    <HeaderAction>
+      <SideNavItems>
+        {#each menuViews as mview}
+        <SideNavMenuItem>
+          <Button on:click={() => view.set(mview)}>{mview}</Button>
+        </SideNavMenuItem>
+        {/each}
+      </SideNavItems>
+    </HeaderAction>
   </Header>
-  <SideNav>
-    <SideNavItems>
-      {#each menuViews as mview}
-      <SideNavMenuItem>
-        <Button on:click={() => view.set(mview)}>{mview}</Button>
-      </SideNavMenuItem>
-      {/each}
-    </SideNavItems>
-  </SideNav>
+  <HashButton />
 </Theme>
 <Content>
   <Grid fullWidth>
